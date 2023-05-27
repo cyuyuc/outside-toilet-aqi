ef06740f55860c93102ae4124074d903bda4090e
cyuyuc <130678477+cyuyuc@users.noreply.github.com>
2023-05-27 21:51:38 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,16 @@ radio.onReceivedNumber(function (receivedNumber) {
     aqi = receivedNumber
 })
 let aqi = 0
+music.stopAllSounds()
 basic.clearScreen()
-radio.setGroup(11)
 basic.showIcon(IconNames.Happy)
 radio.setTransmitPower(7)
+radio.setGroup(11)
+music.setVolume(127)
 basic.forever(function () {
-    basic.showNumber(aqi)
+    if (aqi == 999) {
+        music.playMelody("C5 C5 C C C5 C5 C C ", 350)
+    } else {
+        basic.showNumber(aqi)
+    }
 })
