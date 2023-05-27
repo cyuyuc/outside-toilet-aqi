212271ea951762ddff2b408a3858bd39a596a5af
cyuyuc <130678477+cyuyuc@users.noreply.github.com>
2023-05-27 12:41:40 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    aqi = receivedNumber
+})
+let aqi = 0
+basic.clearScreen()
+radio.setGroup(11)
+basic.showIcon(IconNames.Happy)
+radio.setTransmitPower(7)
+basic.forever(function () {
+    basic.showNumber(aqi)
+})
